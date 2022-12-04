35661520dfb26201a878047fac68fe8a8202a465
Kennedy Francisco <kennedy999francisco@gmail.com>
2022-12-04 14:58:40 -0300

    Finalizado a aplicação de orcamento pessoal

--- script.js
+++ script.js
@@ -52,6 +52,7 @@ class Bd {
             if(despesa === null){
                 continue
             }
+
             despesa.id = i
             despesas.push(despesa)
         }
@@ -93,6 +94,10 @@ class Bd {
         
         return despesasFiltradas
 
+    }
+
+    remover(id) {
+        localStorage.removeItem(id)
     }
 }
 
@@ -187,6 +192,22 @@ function carregarListaDespesas(despesas = Array(), filtro = false) {
         linha.insertCell(1).innerHTML = d.tipo
         linha.insertCell(2).innerHTML = d.descricao
         linha.insertCell(3).innerHTML = d.valor
+
+        // criar o botão de exclusão
+        let btn = document.createElement('button')
+        btn.className = 'btn btn-danger'
+        btn.innerHTML = '<i class="fas fa-times"></i>'
+        btn.id = 'id_despesa' + d.id
+        btn.onclick = function() {
+            // remover a despesa
+
+            let id = this.id.replace("id_despesa", "")
+
+            bd.remover(this.id)
+
+            window.location.reload()
+        }
+        linha.insertCell(4).append(btn)
     })
 }
 
